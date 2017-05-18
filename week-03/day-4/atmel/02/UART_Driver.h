acb810c1803ae6afffddbd6fa853c8ebe7661f15
Peter Incze <peter.incze@gmail.com>
2017-05-18 20:48:05 +0200

    atmel

---
+++ week-03/day-4/atmel/02/UART_Driver.h
@@ -0,0 +1,25 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 2017. 05. 18. 15:02:48
+ *  Author: Peeter
+ */ 
+
+#ifndef _UART_DRIVER_H_
+#define _UART_DRIVER_H_
+
+#define RX_CIRC_BUFF_LEN 25
+
+volatile struct CircularBuffer {
+	char buffer[RX_CIRC_BUFF_LEN];
+	char* head;
+	char* tail;
+	char* write_ptr;
+	char* read_ptr;
+} rx_buffer;
+
+void UART_Init();
+void UART_SendCharacter(char character);
+char UART_GetCharacter();
+
+#endif /* _UART_DRIVER_H_ */
