f2079e98adb60ca3451d05d4f29d2973910f5ab8
Peter Incze <peter.incze@gmail.com>
2017-08-01 16:14:16 +0200

    Frequency and rpm measure

---
+++ week-07/day-2/RPM_measurement/freq_meas.h
@@ -0,0 +1,15 @@
+/*
+ * freq_meas.h
+ *
+ * Created: 2017. 06. 13. 17:21:53
+ *  Author: Peeter
+ */ 
+
+
+#ifndef FREQ_MEAS_H_
+#define FREQ_MEAS_H_
+
+void freq_meas_init();
+float get_freq();
+
+#endif /* FREQ_MEAS_H_ */
---
+++ week-07/day-2/frequency_measurement/freq_meas.h
@@ -0,0 +1,18 @@
+/*
+ * freq_meas.h
+ *
+ * Created: 2017. 06. 13. 11:31:39
+ *  Author: Peeter
+ */ 
+
+
+#ifndef FREQ_MEAS_H_
+#define FREQ_MEAS_H_
+
+
+void freq_meas_init();
+float get_freq();
+
+
+
+#endif /* FREQ_MEAS_H_ */
