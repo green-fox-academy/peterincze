4601895046aaedc860430fa9b43c41de1f82fb84
Peter Incze <peter.incze@gmail.com>
2017-10-16 10:11:04 +0200

    add: (peter) task description

--- Euler/Euler_03/main.c
+++ Euler/Euler_03/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ? */
+
 int main()
 {
     printf("Hello world!\n");
