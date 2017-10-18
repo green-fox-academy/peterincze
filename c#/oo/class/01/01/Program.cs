0e3ca66d10bb8d8d13a435c12dfea5cb823d38f3
Peter Incze <peter.incze@gmail.com>
2017-10-18 16:02:48 +0200

    finished

--- c#/oo/class/01/01/Program.cs
+++ c#/oo/class/01/01/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace _01
 {
+	class Student
+	{
+		public string name;
+		public int age; 
+	}
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Student john = new Student();
+
+			john.name = "John";
+			john.age = 12;
+
+			Console.WriteLine("{0},\t {1}", john.age, john.name);
+			Console.ReadLine();
+
 		}
 	}
 }
