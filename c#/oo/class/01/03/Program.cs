a478d97d808fb9be88dca2899ec4e7ea6e4d802d
Peter Incze <peter.incze@gmail.com>
2017-10-18 22:39:00 +0200

    finished (peter): program03

--- c#/oo/class/01/03/Program.cs
+++ c#/oo/class/01/03/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Írjunk olyan programot, amely számjegyenként bekér egy 3 jegyű kettes számrendszerbeli számot, 
+//majd kiszámolja ezen szám tízes számrendszerbeli alakját.
+
 namespace _03
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			string a, b, c;
+			a = Console.ReadLine();
+			b = Console.ReadLine();
+			c = Console.ReadLine();
+
+			double x, y, z;
+			x = double.Parse(a);
+			y = int.Parse(b);
+			z = int.Parse(c);
+
+			double sum = 0;
+
+			if (x == 1)
+			{
+				sum += Math.Pow(2, 2);
+			}
+			if (y == 1)
+			{
+				sum += Math.Pow(2, 1);
+			}
+			if (z == 1)
+			{
+				sum += Math.Pow(2, 0);
+			}
+			Console.WriteLine("{0}", sum);
+			Console.ReadLine();
 		}
 	}
 }
