89a3c73d4c02b4584f9bc425234833cadb7b4216
Peter Incze <peter.incze@gmail.com>
2017-10-19 11:12:41 +0200

    finished (peter): program04

--- c#/oo/class/01/04/Program.cs
+++ c#/oo/class/01/04/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Create a new instace of the Student class and set it's name to "John" and
+// it's age to 21 with it's constructor
+
+
 namespace _04
 {
+	class Student
+	{
+    public string name;
+	public uint age;
+		public Student(string name, uint age)
+		{
+			this.age = age;
+			this.name = name;
+		}
+	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			Student john = new Student("John", 21);
+			Console.WriteLine("{0}, {1}", john.name, john.age);
+			Console.ReadKey();
 		}
+
 	}
 }
