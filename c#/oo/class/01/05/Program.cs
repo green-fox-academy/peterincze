fea455f46bc808c94666412521d12cfb80d6a278
Peter Incze <peter.incze@gmail.com>
2017-10-19 11:38:55 +0200

    finished (peter): Program05

---
+++ c#/oo/class/01/05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Create a method on the Student class called "greet" that returns a string
+// like: "Hello my name is: <student name>"
+
+
+namespace _05
+{
+	class Student
+	{
+		public string name;
+		public uint age;
+		public void StudentPar(string name, uint age)
+		{
+			this.age = age;
+			this.name = name;
+		}
+		public static void Great(string name)
+		{
+			Console.WriteLine("Hello my name is: {0}", name);
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Student john = new Student();
+			john.StudentPar("John", 21);
+			Student.Great(john.name);
+			Console.ReadKey();
+		}
+	}
+}
